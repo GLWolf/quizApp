7cd518e6293b3329a5d7c8f0631ba5001283d0f5
Tony Bento <bentotony1010@yahoo.com>
2019-07-20 16:23:35 -0500

    added questions

---
+++ questions.js
@@ -0,0 +1,104 @@
+const STORE = [
+    {
+        question:'Who was the first hero to take on the name Green Lantern?',
+        answers: [
+            'Hal Jordon',
+            'Alan Scott',
+            'Guy Gardener',
+            'Jhon Stewart',
+        ],
+        correctAnswer: 'Alan Scott',
+    },
+    {
+        question: 'What space sector is Earth in?',
+        answers: [
+            '2918',
+            '1890',
+            '2814',
+            '2800',
+        ],
+        correctAnswer: '2814',
+    },
+    {
+        question: 'What power from the emotional spectrum does the Green Lantern Corps use?',
+        answers:[
+            'Compassion',
+            'Love',
+            'Hope',
+            'Willpower',
+        ],
+        correctAnswer: 'Willpower',
+    },
+    {
+        question: `Finish the Oath "In brightest day, in blackest night,
+            no evil shall escape my sight. Let those who worship evil's might 
+            ?????????????--GREEN LANTERN'S LIGHT!`,
+        answers:[
+            'beware my power',
+            'fear and hide',
+            'run and fear',
+            'fear my might',
+        ],
+        correctAnswer: 'beware my power',
+    },
+    {
+        question: 'The gardians live on what planet?',
+        answers:[
+            'Oa',
+            'Vogsphere',
+            'Gallifrey',
+            'Krypton',
+        ],
+        correctAnswer: 'Oa',
+    },
+    {
+        question: 'Kyle Rayner was once a ??? before he was Green Lantern.',
+        answers:[
+            'teacher',
+            'test piolt',
+            'musician',
+            'artist',
+        ],
+        correctAnswer: 'artist',
+    },
+    {
+        question: 'For a shor time which member of the Green Lantern Coprs was the Mosaic?',
+        answers:[
+            'Hal Jordon',
+            'Kilowog',
+            `Ch'p`,
+            'Jhon Stewart',
+        ],
+        correctAnswer: 'Jhon Stewart',
+    },
+    {
+        question: 'What is the name of the entity of willpower?',
+        answers:[
+            'Ion',
+            'Parallax',
+            'Adara<br',
+            'Proselyte',
+        ],
+        correctAnswer: 'Ion',
+    },
+    {
+        question: 'Whic of these Green Lanterns is also a living planet?',
+        answers:[
+            'Katma Tui',
+            'Mogo',
+            `Ke'Haan`,
+            'Abin Sur',
+        ],
+        correctAnswer: 'Mogo',
+    },
+    {
+        question: 'Which of these Corps boost the power of the Green Lantern with them?',
+        answers:[
+            'Indigo tribe',
+            'Star Sapphire',
+            'Blue Lantern',
+            'Red Lantern',
+        ],
+        correctAnswer: 'Blue Lantern'
+    }
+];
